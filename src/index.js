cd62370ea432b4e9178edbd1f1a4944cf5f4c70b
seongyurim <miruyseong@gmail.com>
2024-09-04 14:56:24 +0900

    commit at 1456

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const queryClient = new QueryClient();
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <BrowserRouter basename="/netflip-streaming-app-2024">
       <App />
     </BrowserRouter>
   </QueryClientProvider>
